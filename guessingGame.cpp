3fba9b13b9ca6c59cb88cf45fb77b507044cc2f5
Varmais1 <418487@bsd48.org>
2022-09-14 16:51:35 -0700

    1 play

--- guessingGame.cpp
+++ guessingGame.cpp
@@ -1,8 +1,43 @@
+/*Collaborator(s): Ishaan Varma
+What this file does: Has all the code to make the guessing game functional
+Date: 9/14/2022
+ */
+//adds the console input and output
 #include <iostream>
-
+//makes it so that I don't have to type std:: before cin and cout
 using namespace std;
 
+//main
 int main()
 {
-  cout << "Working.";
+  //sets the seed to the default seed, which changes constantly
+  srand(time(NULL));
+  //print the rules
+  cout << "This is a guessing game." << endl;
+  cout << "Guess a number between 0 and 100 inclusive." << endl;
+  //generate random num between 0 and 100
+  int number = rand() % 101;
+  //variable for user's guess
+  int userGuess = 101;
+  //variable for number of guesses
+  int guesses = 0;
+  //for testing purposes
+  cout << "number: " << number << endl;
+  //while the users guess isn't right...
+  while(userGuess != number) {
+    //increment the num of guesses by one
+    guesses++;
+    //get the user's guess
+    cin >> userGuess;
+    //tells the user if their number is too high or too low
+    if(userGuess > number) {
+      cout << "Your guess was too high." << endl;
+    }
+    else if(userGuess < number) {
+      cout << "Your guess was too low." << endl;
+    }
+  }
+  cout << "You guessed correct!" << endl;
+  cout << "You took " << guesses << " guesses." << endl;
+  
 }
